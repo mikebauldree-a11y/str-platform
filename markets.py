595fc51edb208d8703b8459bd9dba31b4874f115
Mike Bauldree <mikebauldree@MacBook-Pro-2.lan>
2026-05-07 17:36:16 -0400

    Add /markets/newmarket route and template

--- markets.py
+++ markets.py
@@ -5,3 +5,8 @@ markets_bp = Blueprint("markets", __name__, template_folder="templates")
 @markets_bp.route("/aruba")
 def aruba():
     return render_template("markets/aruba.html")
+
+# Route added to serve the new market page
+@markets_bp.route("/newmarket")
+def newmarket():
+    return render_template("markets/newmarket.html")
